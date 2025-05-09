59567c15140c388d55bb0083207ae5b266ce15db
acarbonilla <carbonillaalfredo@gmail.com>
2025-05-09 09:08:35 +0800

    Removing Sheild

--- oms/settings.py
+++ oms/settings.py
@@ -10,7 +10,7 @@ env = environ.Env(
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Read .env file
-environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
+# environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
